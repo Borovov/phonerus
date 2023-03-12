ae4dd354e04c987f32b39b421b1f82ad7c3406a5
Боровов Максим Сергеевич <maxim.borovov@corp.codeinside.ru>
2023-03-12 17:19:04 +0300

    init

---
+++ src/test/java/com/resin/phonerus/PhonerusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resin.phonerus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonerusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
